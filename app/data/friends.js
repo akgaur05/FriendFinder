4545f74640628b71b00d9dfb9febf7ff2b7ba544
Akanksha <agaur05@gmail.com>
2018-11-11 00:31:26 -0600

    friend finder files

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+let friends = [
+   {
+      "name":"Shikher The One",
+      "photo":"http://www.piliving.com/wp-content/uploads/2009/08/scary-300x287.jpg",
+      "scores":[5,1,4,4,5,1,2,5,4,1]
+   },
+   {
+      "name":"Stripes Love",
+      "photo":"https://s1.gifyu.com/images/arkavilon-body5.jpg",
+      "scores":[4,2,5,1,3,2,2,1,3,2]
+   },
+   {
+      "name":"Joel Sart",
+      "photo":"https://www.joelsartore.com/assets/stock/ANI105/ANI105-00038-400x267.jpg?x21176",
+      "scores":[5,2,2,2,4,1,3,2,5,5]
+   },
+   {
+      "name":"Laila",
+      "photo":"https://irp-cdn.multiscreensite.com/b130d8f2/dms3rep/multi/mobile/Llama-500x333.jpg",
+      "scores":[3,3,4,2,2,1,3,2,2,3]
+   },
+	{
+	   "name":"Gor Ila",
+	   "photo":"http://pravda-team.ru/eng/image/article/5/5/9/60559.jpeg",
+	   "scores":[4,3,4,1,5,2,5,3,1,4]
+	},
+      {
+      "name":"Pan Haider",
+      "photo":"https://timedotcom.files.wordpress.com/2017/01/cute-animal-tweet-off-zoo.jpg?quality=85",
+      "scores":[3,3,4,1,4,3,5,3,5,4]
+   },
+	{
+	   "name":"Clown",
+	   "photo":"https://images.8tracks.com/cover/i/002/067/033/Erwin-Olaf-Sad-Clown-8690.jpg?rect=0,109,670,670&q=98&fm=jpg&fit=max&w=640&h=640",
+	   "scores":[4,4,2,3,2,2,3,2,4,5]
+	}
+
+]
+
+module.exports = friends;
